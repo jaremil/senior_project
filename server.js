1ba003ddfb9433bd7f37c75f60221c8daad0fe00
Jade Miller <1jaremil1@gmail.com>
2025-07-14 15:39:56 -0600

    connecting to render

---
+++ server.js
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const passport = require('passport');
+const session = require('express-session');
+// const mongoose = require('mongoose');
+const express = require('express');
+var app = express();
+require('./data/google_auth.js');
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+});
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(express.static('public'));
+
+const projectsRouter = require('./data/routes.js')
+app.use('/', projectsRouter);
