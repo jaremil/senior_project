e3c7d84c0c3475d73004d7149d71e373fa897e38
Jade Miller <1jaremil1@gmail.com>
2025-07-18 14:42:58 -0600

    j

--- data/connection.js
+++ data/connection.js
@@ -4,7 +4,10 @@ mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 
-const User = mongoose.model('User', { displayName: String, googleId: String, email: String })
-const Recipe = mongoose.model('Recipe', { recipeName: String, displayName: String, });
+const User = mongoose.model('User', { displayName: String, googleId: String, email: String });
+const Recipe = mongoose.model('Recipe', { recipeName: String, userId: Object, ingredients: Array, instructions: Array });
+const Comments = mongoose.model('Comments', { general: Boolean, public: Boolean });
+const Ingredient = mongoose.model('Ingredient', { ingredientName: String, groceryList: String,
+inventoryList: String });
 
-module.exports = { User, Recipe }
+module.exports = { User, Recipe, Comments, Ingredient }
